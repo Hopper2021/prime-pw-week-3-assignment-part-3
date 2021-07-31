3c7084046ede078ffa8d9775a623f3646ee0b18b
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-07-30 21:37:07 -0500

    Answered questions 7 and 8, attempted 9

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -46,12 +46,41 @@ for ( let i=0; i<supplyChanges.length; i++ ){
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
+
 console.log('7. Showing supplyChanges with "for of" loop');
+for ( number of supplyChanges ){
+  if ( number > 0 ){
+    console.log( 'Added ', number, ' parts.' );
+  } else if ( number === 0 ){
+    console.log( 'No change.' );
+  } else if ( number < 0 ) {
+    console.log( 'Removed ', number, ' parts.' );
+  }
+}
 
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
 console.log('8. Showing supplyChanges with "while" loop');
+i = 0;
 
+while ( i < supplyChanges.length ){
+  if ( supplyChanges[i] > 0 ){
+    console.log( 'Added ', i, ' parts.' );
+    i++;
+  } else if ( supplyChanges[i] === 0 ){
+    console.log( 'No change.' );
+    i++;
+  } else if ( supplyChanges[i] < 0 ) {
+    console.log( 'Removed ', supplyChanges[i], ' parts.' );
+    i++;
+  }
+}
 
 // 9. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('9. Total supplies available is:');
+let sum = 0;
+
+for ( let i=0; i<supplyChanges.length; i++ ){
+  sum += supplyChanges.length[i];
+console.log( 'Total parts available: ', sum);
+}
